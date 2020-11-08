4597e7a1b6e8798da75bd4b3d69afdbce78c17b1
hpca01 <hirenpatelca@gmail.com>
2020-11-08 10:03:50 -0800

    added port, and giving an address to open socket.

--- server.c
+++ server.c
@@ -1,6 +1,7 @@
 #include <sys/socket.h>
 #include <errno.h>
 #include <netinet/in.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
@@ -18,23 +19,34 @@ int main(int argc, char const *argv[])
         binding to socket requires a socketaddr struct,
         struct is a generic, the actual definition is determined byt the type of network
         IP networking:
+        https://pubs.opengroup.org/onlinepubs/009695399/basedefs/netinet/in.h.html
+
         struct sockaddr_in
         {
             __uint8_t sin_len; //note this is specific to linux...not portable
             sa_family_t sin_family;
             in_port_t sin_port;
             struct in_addr sin_addr;
-            char sin_zero[8];
+            char sin_zero[8]; //technically removed...see link
         };
     */
 
     struct sockaddr_in address;
     const int PORT_NUM = 8080; //where client goes
 
-    memset((char *)&address, 0, sizeof(address));
-    address.sin_family = AF_INET; //0.0.0.0
+    memset((char *)&address, 0, sizeof(address)); //set everything to 0, basically initialize to 0
+    address.sin_family = AF_INET;                 //0.0.0.0
     address.sin_addr.s_addr = htonl(INADDR_ANY);
     address.sin_port = htons(PORT_NUM);
+
+    int bind_result = bind(server_file_d, (struct sockaddr *)&address, sizeof(address));
+
+    if (bind_result < 0)
+    {
+        //failed to bind correctly
+        perror("Failed to bind socket to address/port");
+        return 0;
+    }
 
     return 0;
 }
